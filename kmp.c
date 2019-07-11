ea003a368cca5da8c4b167c8626e518047556c5c
freecode12 <49920678+freecode12@users.noreply.github.com>
2019-07-11 08:12:17 +0530

    Update kmp.c

--- kmp.c
+++ kmp.c
@@ -1,57 +1,63 @@
 #include<stdio.h>
 #include<string.h>
-voidfail(char*pat,intfailure[])
+#include<stdlib.h>
 
-{
-inti,j,n=strlen(pat);
-failure[0]=-1;
-for(j=1;j<n;j++)
-{
-i=failure[j-1];
-while((pat[j]!=pat[i+1])&&(i>=0))
-i=failure[i];
-if(pat[j]==pat[i+1])
-failure[j]=i+1;
+int failure[100];
 
-else
+void fail(char *pat)
+{
+    int i,j;
+    int n = strlen(pat);
+    failure[0] = -1;
+    for(j=1;j<n;j++)
+    {
+        i = failure[j-1];
+        while(pat[j] != pat[i+1] && i>=0)
+            i=failure[i];
+        if(pat[j]==pat[i+1])
+            failure[j]=i+1;
+        else
+            failure[j]=-1;
+    }
+}
 
-failure[j]=-1;
+int pmatch(char *string, char *pat)
+{
+    int i=0, j=0;
+    int lens = strlen(string);
+    int lenp = strlen(pat);
+    while(i<lens && j<lenp)
+    {
+        if(string[i]==pat[j])
+        {
+            i++;
+            j++;
+        }
+        else if(j==0)
+            i++;
+        else j=failure[j-1]+1;
+    }
+    return ((j==lenp)?(i-lenp):-1);
+}
 
-}
-}
-intpmatch(char*string,char*pat,intfailure[])
+void main()
 {
-inti=0,j=0;
-intlens=strlen(string);
-intlenp=strlen(pat);
-while(i<lens&&j<lenp)
-{
-if(string[i]==pat[j])
-{
-i++;
-j++;
-}
-elseif(j==0)
-i++;
-else
-j=failure[j-1]+1;
+    int n,i;
+    char s1[100], s2[100];
+    printf("Enter the string");
+    gets(s1);
+    printf("Enter the pattern");
+    gets(s2);
+    puts(s1);
+    puts(s2);
+    fail(s2);
+    for(i=0; i<strlen(s2); i++)
+        printf("%d ",failure[i]);
+    printf("\n");
+    n=pmatch(s1, s2);
+    if(n==-1)
+        printf("Not found");
+    else
+        printf("Found at %d", n);
 
-}
-return((j==lenp)?(i-lenp):-1);
-}
-intmain()
-{
-charstring[50],pat[50];
-intch,i;
-intfailure[100];
-printf("enterthestring\n");
-scanf("%s",string);
-printf("enterpattern\n");
-scanf("%s",pat);
-fail(pat,failure);
-printf("%s\n",pat);
-for(i=0;i<strlen(pat);i++)
-printf("%d",failure[i]);
-printf("\n");
-printf("posofmatchis%d\n",pmatch(string,pat,failure)+1);
 }
