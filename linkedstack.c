cc54ec162e40946e4c0401d4201f3f5183131701
freecode12 <49920678+freecode12@users.noreply.github.com>
2019-07-11 08:21:25 +0530

    Update linkedstack.c

--- linkedstack.c
+++ linkedstack.c
@@ -1,86 +1,89 @@
-#include<stdio.h>
-#include<stdlib.h>
-typedefstructelement
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct
 {
-intkey;
+	int key;
 }element;
-structstack
+
+struct node
 {
-elementdata;
-structstack*link;
+	element data;
+	struct node* link;
 };
-typedefstructstack*stackpointer;
 
-stackpointertop[10];
-voidpush(elementitem,inti)
-{
-stackpointertemp;
-temp=(stackpointer)malloc(sizeof(structstack));
-temp->data=item;
-temp->link=top[i];
-top[i]=temp;
-}
-elementpop(inti)
-{
-stackpointertemp=top[i];
-elementitem;
-if(!temp)
-{
-item.key=-1;
-returnitem;
-}
-item=top[i]->data;
-top[i]=top[i]->link;
-free(temp);
-returnitem;
-}
-voiddisplay(inti)
-{
-stackpointertemp;
-temp=top[i];
-if(top[i]==NULL)
-{
-printf("stackisempty\n");
-return;
-}
-printf("stack%dis\n",i+1);
-for(;temp;temp=temp->link)
-printf("%d\n",temp->data.key);
-printf("\n");
-}
-intmain()
-{
-intstackno,choice;
-elementitem;
-while(1)
-{
-printf("Enter\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
-scanf("%d",&choice);
-if(choice!=4)
-{
-printf("Enterstacknumberfrom1-10\n");
-scanf("%d",&stackno);
-}
+typedef struct node* stackptr;
 
-switch(choice)
+stackptr stack[10]={NULL};
+
+void insert(element item, int index)
 {
-case1:printf("Enterdatatobeinserted:");
-scanf("%d",&item.key);
-push(item,stackno-1);
-break;
-case2:item=pop(stackno-1);
-if(item.key==-1)
-printf("Stackempty\n");
-else
-printf("Elementdeleted:%d\n",item.key);
-break;
-case3:display(stackno-1);
-break;
-
-case4:printf("OperationComplete\n");
-
-exit(0);
+	stackptr temp;
+	temp=(stackptr)malloc(sizeof(struct node));
+	temp->data=item;
+	temp->link=stack[index];
+	stack[index]=temp;
 
 }
+
+element deletes(int index)
+{
+	element item;
+	if(stack[index])
+	{
+		item=stack[index]->data;
+		stack[index]=stack[index]->link;
+	}
+	else
+	{
+		printf("Stack empty");
+		item.key=-1;
+	}
+	return item;
 }
+
+void display(int index)
+{
+	stackptr temp=stack[index];
+	if(!temp)
+	{
+		printf("Stack empty");
+		return;
+	}
+	for(;temp;temp=temp->link)
+		printf("%d\t",temp->data.key);
+}
+
+int main()
+{
+	int index, choice;
+	element item;
+	while(1)
+	{
+		printf("Enter 1. Insert 2. Delete 3. Display\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+		case 1:
+			printf("Enter stack index: ");
+			scanf("%d",&index);
+			printf("Enter element to be inserted");
+			scanf("%d",&item.key);
+			insert(item, index);
+			break;
+		case 2:
+			printf("Enter stack index: ");
+			scanf("%d",&index);
+			item=deletes(index);
+			if(item.key!=-1)
+				printf("Element deleted %d", item.key);
+			break;
+		case 3:
+			printf("Enter stack index: ");
+			scanf("%d",&index);
+			display(index);
+
+
+		}
+	}
+
 }
